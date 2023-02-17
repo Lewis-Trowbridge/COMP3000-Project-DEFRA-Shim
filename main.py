98e161938a0fd93c49893f7ebbd3b882d107e39e
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-02-17 12:49:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import datetime
+from fastapi import FastAPI, Response
+from pyaurn import importAURN
+
+app = FastAPI()
+
+
+@app.get("/data")
+async def test2(site: str, date: datetime.datetime):
+    data = importAURN(site, [date.year])
+    if not data.empty and date in data.index:
+        return Response(content=data.loc[date].to_json(orient="index", date_format="iso"),
+                        media_type="application/json")
+    else:
+        return Response(status_code=404)
